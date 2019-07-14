c9333f03315761c08d8a782989c4feb0009c8890
SuneClausen <sunekimclausen@hotmail.com>
2019-07-14 16:45:36 +0200

    started on a static utility class. For now there is only naive implementation of distance in 2d given 2 points

--- src/Utility.java
+++ src/Utility.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Utility {
+
+    //TODO: Support any dimension. Now it just supports two dim.
+    //Calculate the euclidian distance
+    public static double distance(Point point1, Point point2) {
+
+        double temp = (point1.getX() - point2.getX())^2 + (point1.getY() - point2.getY())^2;
+
+        return Math.sqrt(temp);
+    }
+
+
 }
