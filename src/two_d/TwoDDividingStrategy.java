6ea9234fc5c18049eda476b3c1c20a52fb917edf
SuneClausen <sunekimclausen@hotmail.com>
2019-08-11 12:22:02 +0200

    Cleancoded two strategies

--- src/two_d/TwoDDividingStrategy.java
+++ src/two_d/TwoDDividingStrategy.java
@@ -13,16 +13,13 @@ public class TwoDDividingStrategy implements DividingStrategy {
         List<Point> pointsByX = points.get(0);
         List<Point> pointsByY = points.get(1);
 
-        //TODO write better
-        int medianIndexXValue = points.get(0).get(medianIndex).getIndex().get("x"); //get the given index of the medianpoint at the median point index.
-        medianIndex = medianIndexXValue;
-
+        int median = points.get(0).get(medianIndex).getIndex().get("x"); //get the given index of the medianpoint at the median point index.
 
         // Get the x set
         List<Point> pointsByX_Left = new ArrayList<>();
         List<Point> pointsByX_Right = new ArrayList<>();
         for (Point point : pointsByX) {
-            if (point.getIndex().get("x") <= medianIndex) { //get(0) gets the index of x
+            if (point.getIndex().get("x") <= median) { //get(0) gets the index of x
                 pointsByX_Left.add(point);
             } else {
                 pointsByX_Right.add(point);
@@ -33,7 +30,7 @@ public class TwoDDividingStrategy implements DividingStrategy {
         List<Point> pointsByY_Left = new ArrayList<>();
         List<Point> pointsByY_Right = new ArrayList<>();
         for (Point point : pointsByY) {
-            if (point.getIndex().get("x") <= medianIndex) {
+            if (point.getIndex().get("x") <= median) {
                 pointsByY_Left.add(point);
             } else {
                 pointsByY_Right.add(point);
@@ -47,7 +44,6 @@ public class TwoDDividingStrategy implements DividingStrategy {
         rigth.add(pointsByX_Right);
         rigth.add(pointsByY_Right);
 
-//        return new List[]{left, rigth};
         return new DividingValuesHelper(left, rigth);
     }
 }
